d9606cd2e546c209fc42a80e9f0a5c8875962b2b
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-04-29 11:01:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ bot.on("message", async message => {
   let args = messageArray.slice(1);
 
   if(cmd === `${prefix}verify`){
-    let gRole = message.guild.roles.find(`name`, Members)
+    let gRole = message.guild.roles.find(`name`, "Members")
     
    if(message.author.roles.has(gRole.id)) return message.reply("They already have that role.");
   await(message.author.addRole(gRole.id));
